976d5c3c05752e414f724f5e04c9c232be88f86c
Jeff Potts <jeff@2020creative.com>
2014-12-15 12:21:25 -0600

    wip

--- header.php
+++ header.php
@@ -8,7 +8,20 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
 <?php wp_head(); ?>
 <script src="//use.typekit.net/ndy3syp.js"></script>
-<script>try{Typekit.load();}catch(e){}</script>    
+<script>try{Typekit.load();}catch(e){}</script>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1342678-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 <div class="container-fluid">

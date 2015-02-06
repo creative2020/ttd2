9bf288b204029f6e3b3bf4fed08d3b339c0c17b3
Jeff Potts <jeff@2020creative.com>
2015-02-06 15:21:52 -0600

    wip

--- home.php
+++ home.php
@@ -11,15 +11,18 @@ Template Name: Home
     </div>
 </div> <!--row-->
 
-<div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
+
+
+<div class="maxpg">
+    
+    <div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
         <?php echo do_shortcode('[image-carousel showcaption="false"]'); ?>    
     </div>
 
         
 </div> <!--row-->
-
-<div class="maxpg">
+    
 <div class="row">
     <div id="main" class="col-sm-6 col-sm-offset-1">
         <div id="content" class="row col-sm-12">
